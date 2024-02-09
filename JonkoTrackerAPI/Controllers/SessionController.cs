9a9ef8188a9e6f590b56aa25c2ea4f7828c0b799
ZoutigeWolf <guuskamphuis@gmail.com>
2024-02-09 13:08:03 +0100

    session jonkos + pictures

--- JonkoTrackerAPI/Controllers/SessionController.cs
+++ JonkoTrackerAPI/Controllers/SessionController.cs
@@ -10,7 +10,7 @@ namespace JonkoTrackerAPI.Controllers;
 [Route("api/sessions")]
 public class SessionController : Controller<SessionController, SessionHandler>
 {
-    public SessionController(ILogger<SessionController> logger, DatabaseContext context, IConfiguration configuration) 
+    public SessionController(ILogger<SessionController> logger, DatabaseContext context, IConfiguration configuration)
         : base(logger, context, configuration)
     {
     }
@@ -18,7 +18,7 @@ public class SessionController : Controller<SessionController, SessionHandler>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<IEnumerable<Session>> GetAllSessions() => Handler.GetAllSessions();
-    
+
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -28,4 +28,32 @@ public class SessionController : Controller<SessionController, SessionHandler>
     [Authorize]
     public ActionResult<Session> CreateSession(SessionCreationData data) =>
         Handler.CreateSession(GetUserFromClaims(), data);
+
+    [HttpGet("{id:int}/pictures/{n:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> GetSessionPictures(int id, int n) =>
+        await Handler.GetPicture(GetUserFromClaims(), id, n);
+
+    [HttpPost("{id:int}/pictures/{n:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> UploadSessionPictures(int id, int n, int size) =>
+        await Handler.UploadPicture(GetUserFromClaims(), id, n, size, Request.BodyReader.AsStream());
+
+    [HttpGet("{id:int}/jonkos")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<Jonko>> GetSessionJonkos(int id) => Handler.GetJonkos(id);
+
+    [HttpGet("{id:int}/jonkos/{jonkoId:int}/picture")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> GetSessionJonkoPicture(int id, int jonkoId) =>
+        await Handler.GetJonkoPicture(GetUserFromClaims(), id, jonkoId);
+
+    [HttpGet("{id:int}/jonkos/{jonkoId:int}/picture")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> UploadSessionJonkoPicture(int id, int jonkoId, int size) =>
+        await Handler.UploadJonkoPicture(GetUserFromClaims(), id, jonkoId, size, Request.BodyReader.AsStream());
 }
--- JonkoTrackerAPI/Handlers/SessionHandler.cs
+++ JonkoTrackerAPI/Handlers/SessionHandler.cs
@@ -1,6 +1,4 @@
-using System.Text.RegularExpressions;
 using JonkoTrackerAPI.Models;
-using JonkoTrackerAPI.Services;
 using JonkoTrackerAPI.Types;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.Configuration;
@@ -9,10 +7,15 @@ namespace JonkoTrackerAPI.Handlers;
 
 public class SessionHandler : Handler
 {
+    private readonly string _sessionBucket;
+    private readonly string _jonkoBucket;
+    
     public SessionHandler(ILogger logger, DatabaseContext context, IConfiguration configuration) 
         : base(logger, context, configuration)
     {
-        
+        this._sessionBucket = Configuration["Storage:Buckets:Sessions"] ?? throw new InvalidConfigurationException();
+        this._jonkoBucket = Configuration["Storage:Buckets:Jonkos"] ?? throw new InvalidConfigurationException();
+
     }
 
     public ActionResult<IEnumerable<Session>> GetAllSessions()
@@ -46,5 +49,155 @@ public class SessionHandler : Handler
             "Session", 
             new { id = session.Id }, 
             session);
+    }
+    
+    public async Task<ActionResult> GetPicture(User? user, int id, int n)
+    {
+        if (user == null)
+        {
+            return new UnauthorizedResult();
+        }
+
+        Session? session = Services.Sessions.GetById(id);
+        
+        if (session == null)
+        {
+            return new NotFoundResult();
+        }
+
+        if (session.UserId != user.Id)
+        {
+            return new ForbidResult();
+        }
+
+        Stream? stream = await Services.Storage.Get(_sessionBucket, $"{id.ToString()}_{n.ToString()}.png");
+
+        if (stream == null)
+        {
+            return new NotFoundResult();
+        }
+
+        return new FileStreamResult(stream, "application/octet-stream");
+    }
+
+    public async Task<ActionResult> UploadPicture(User? user, int id, int n, int size, Stream stream)
+    {
+        if (user == null)
+        {
+            return new UnauthorizedResult();
+        }
+
+        Session? session = Services.Sessions.GetById(id);
+        
+        if (session == null)
+        {
+            return new NotFoundResult();
+        }
+
+        if (session.UserId != user.Id)
+        {
+            return new ForbidResult();
+        }
+        
+        if (await Services.Storage.Get(_sessionBucket, $"{id.ToString()}_{n.ToString()}.png") != null)
+        {
+            return new ConflictResult();
+        }
+        
+        await Services.Storage.Upload(_sessionBucket, $"{id.ToString()}_{n.ToString()}.png", stream, size);
+        await stream.DisposeAsync();
+
+        return new OkResult();
+    }
+
+    public ActionResult<IEnumerable<Jonko>> GetJonkos(int id)
+    {
+        Session? session = Services.Sessions.GetById(id);
+
+        if (session == null)
+        {
+            return new NotFoundResult();
+        }
+
+        Services.LoadCollection(session, s => s.Jonkos);
+
+        return new OkObjectResult(session.Jonkos);
+    }
+    
+    public async Task<ActionResult> GetJonkoPicture(User? user, int sessionId, int jonkoId)
+    {
+        if (user == null)
+        {
+            return new UnauthorizedResult();
+        }
+
+        Session? session = Services.Sessions.GetById(sessionId);
+        
+        if (session == null)
+        {
+            return new NotFoundResult();
+        }
+
+        if (session.UserId != user.Id)
+        {
+            return new ForbidResult();
+        }
+        
+        Services.LoadCollection(session, s => s.Jonkos);
+
+        Jonko? jonko = session.Jonkos.FirstOrDefault(j => j.Id == jonkoId);
+
+        if (jonko == null)
+        {
+            return new NotFoundResult();
+        }
+
+        Stream? stream = await Services.Storage.Get(_jonkoBucket, $"{jonkoId.ToString()}.png");
+
+        if (stream == null)
+        {
+            return new NotFoundResult();
+        }
+
+        return new FileStreamResult(stream, "application/octet-stream");
+    }
+
+    public async Task<ActionResult> UploadJonkoPicture(User? user, int sessionId, int jonkoId, int size, Stream stream)
+    {
+        if (user == null)
+        {
+            return new UnauthorizedResult();
+        }
+
+        Session? session = Services.Sessions.GetById(sessionId);
+        
+        if (session == null)
+        {
+            return new NotFoundResult();
+        }
+
+        if (session.UserId != user.Id)
+        {
+            return new ForbidResult();
+        }
+
+        Services.LoadCollection(session, s => s.Jonkos);
+
+        Jonko? jonko = session.Jonkos.FirstOrDefault(j => j.Id == jonkoId);
+
+        if (jonko == null)
+        {
+            return new NotFoundResult();
+        }
+        
+        if (await Services.Storage.Get(_jonkoBucket, $"{jonkoId.ToString()}.png") != null)
+        {
+            return new ConflictResult();
+        }
+        
+        await Services.Storage.Upload(_jonkoBucket, $"{jonkoId.ToString()}.png", stream, size);
+        await stream.DisposeAsync();
+
+        return new OkResult();
     }
 }
